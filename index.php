085cc146dab1abaa43b1c663619285a11e25e880
Michael Rolli <michael@rollis.ch>
2024-03-18 16:43:17 +0100

    feat: translate weekdays to German

--- index.php
+++ index.php
@@ -2,5 +2,15 @@
 
 date_default_timezone_set('Europe/Zurich');
 
+$weekdays = array(
+  'Monday' => 'Montag',
+  'Tuesday' => 'Dienstag',
+  'Wednesday' => 'Mittwoch',
+  'Thursday' => 'Donnerstag',
+  'Friday' => 'Freitag',
+  'Saturday' => 'Samstag',
+  'Sunday' => 'Sonntag'
+);
+
 echo "Hello World!<br />";
-echo sprintf("Es ist %s, der %s, Uhrzeit: %s", date("l"), date("d.m.Y"), date("H:i:s"));
+echo sprintf("Es ist %s, der %s, Uhrzeit: %s", $weekdays[date("l")], date("d.m.Y"), date("H:i:s"));
